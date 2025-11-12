e86e92cd60c214cf422e372acc38b30bad0d5271
Dmitry-polyakov <dmitry-polyakov111@gmail.com>
2025-11-13 00:09:01 +0300

    Переписана конфигурация package.json

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,17 +1,19 @@
-import { createBot } from './bot';
-import { registerStartCommand } from './commands/start';
+import { createBot } from './bot';  
+import { registerStartCommand } from './commands/start';  
+import { appConfig } from './config';  
+import { logger } from './utils/logger';  
 
-async function main() {
-  const bot = createBot();
+async function main() {  
+  const bot = createBot();  
+  registerStartCommand(bot);  
+  logger('Bot Token:', appConfig.BOT_TOKEN);  
+  await bot.api.getMe();  
+  logger('Bot is starting...');  
+  bot.start();  
+}  
 
-  registerStartCommand(bot);
-
-  await bot.api.getMe();
-  console.log('Bot is starting...');
-  bot.start();
-}
-
-main().catch((err) => {
-  console.error('Fatal error', err);
-  process.exit(1);
+main().catch((err) => {  
+  logger('Fatal error', 'error');  
+  console.error(err);  
+  process.exit(1);  
 });
